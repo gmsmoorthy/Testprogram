7bff15362449f5785eb07c374c2de4915acc98e1
gmsmoorthy <gmsmoorthy28@gmail.com>
2021-01-26 15:24:48 +0530

    New simple program

---
+++ src/org/testprogram/Samplegit.java
@@ -0,0 +1,14 @@
+package org.testprogram;
+
+public class Samplegit {
+	public static void main(String[] args) {
+		int a=10;
+		int b= 05;
+		int c=a+b;
+		System.out.println(c);
+		int d=c*2;
+		System.out.println(d);
+		
+	}
+
+}
